f197565c490eb872e1ba29e18bea0e526b471ac4
lukaqwe <alexeluca.spataru@gmail.com>
2020-04-26 17:41:21 +0300

    Add result display

--- encode.py
+++ encode.py
@@ -5,13 +5,17 @@ from huffman import HuffmanCoding
 # input file path
 path = "bytecode.txt"
 
+total_len = 0
+
 h = HuffmanCoding()
 for filename in os.listdir("contracts"):
     with open("contracts/" + filename, 'r') as file:
         text = file.read().rstrip()
+        total_len += len(text)
         h.make_frequency_dict(text)
 for filename in os.listdir("contracts"):
     path = "contracts/" + filename
     h.compress(path)
 
-print(h.saved)
+print("Total chars saved: " + str(h.saved))
+print("Compression rate: " + str(h.saved * 100/total_len) + "%")
